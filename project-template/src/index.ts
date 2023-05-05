1124777ef21f957fe10978d6990248af683043dc
Nataly <kovalevskaya.nataly@gmail.com>
2023-05-05 21:47:28 +0500

    Домашнее задание по 5 уроку

--- project-template/src/index.ts
+++ project-template/src/index.ts
@@ -1,21 +1,34 @@
-import { renderSearchFormBlock, callback, collectSearchParams, search } from './search-form.js'
-import { renderSearchStubBlock } from './search-results.js'
-import { renderUserBlock, getUserData, getFavoritesAmount } from './user.js'
-import { renderToast } from './lib.js'
+import { callback, collectSearchParams, search } from "./search.js";
+import { renderSearchStubBlock } from "./search-results.js";
+import {
+  getFavoritesAmount,
+  getUserData,
+  renderUserBlock,
+} from "./user-component.js";
 
+import { renderToast, replacer, reviver } from "./lib.js";
+import { renderSearchFormBlock } from "./search-form-render.js";
 
-window.addEventListener('DOMContentLoaded', () => {
-  renderUserBlock(getUserData(), getFavoritesAmount())
-  renderSearchFormBlock()
-  renderSearchStubBlock()
+window.addEventListener("DOMContentLoaded", () => {
+  renderUserBlock(getUserData(), getFavoritesAmount());
+  renderSearchFormBlock();
+  renderSearchStubBlock();
   renderToast(
-    { text: 'Это пример уведомления. Используйте его при необходимости', type: 'success' },
-    { name: 'Понял', handler: () => { console.log('Уведомление закрыто') } }
-  )
+    {
+      text: "Это пример уведомления. Используйте его при необходимости",
+      type: "success",
+    },
+    {
+      name: "Понял",
+      handler: () => {
+        console.log("Уведомление закрыто");
+      },
+    }
+  );
   document
     .getElementById("search-form-block")
     .addEventListener("submit", (e) => {
       e.preventDefault();
       search(collectSearchParams(), callback);
     });
-})
+});
--- project-template/src/search-results.ts
+++ project-template/src/search-results.ts
@@ -1,46 +1,62 @@
-import { placesCoordinates, renderBlock, renderToast } from "./lib.js";
-import { collectSearchParams, Place, SearchFormData } from "./search-form.js";
+import { FlatRentSdk } from "./libraries/flat-rent-sdk/flat-rent-sdk.js";
+import { renderBlock, renderToast, sortType } from "./lib.js";
+import { Place, SearchFormData } from "./search.js";
+import { renderSearchResultsBlock } from "./search-results-render.js";
 
 export interface FavouritePlace {
-  id: number;
+  id: string;
   name: string;
   img: string;
 }
 
-export function renderSearchStubBlock () {
+export interface BookResult {
+  type: "success" | "error";
+  hotelName?: string;
+  checinDate?: Date;
+  checkoutDate?: Date;
+}
+
+export enum Provider {
+  Homy = "Homy",
+  FlatRent = "FlatRent",
+}
+
+export function renderSearchStubBlock(): void {
   renderBlock(
-    'search-results-block',
+    "search-results-block",
     `
     <div class="before-results-block">
       <img src="img/start-search.png" />
       <p>Чтобы начать поиск, заполните форму и&nbsp;нажмите "Найти"</p>
     </div>
     `
-  )
+  );
 }
 
-export function renderEmptyOrErrorSearchBlock (reasonMessage) {
+export function renderEmptyOrErrorSearchBlock(reasonMessage: string): void {
   renderBlock(
-    'search-results-block',
+    "search-results-block",
     `
     <div class="no-results-block">
       <img src="img/no-results.png" />
       <p>${reasonMessage}</p>
     </div>
     `
-  )
+  );
 }
 
 export function toggleFavoriteItem(place: FavouritePlace): void {
   const favoritesElements = document.querySelectorAll(".favorites");
 
-  const localStorageFavouriteItems: FavouritePlace[] = JSON.parse(localStorage.getItem("favoriteItems"));
+  const localStorageFavouriteItems: FavouritePlace[] = JSON.parse(
+    localStorage.getItem("favoriteItems")
+  );
   if (!localStorageFavouriteItems) {
     const favoriteItems: FavouritePlace[] = [];
     favoriteItems.push(place);
     localStorage.setItem("favoriteItems", JSON.stringify(favoriteItems));
     favoritesElements.forEach((el: HTMLElement) => {
-      if (+el.dataset.id === +place.id) {
+      if (String(el.dataset.id) === String(place.id)) {
         el.classList.add("active");
       }
     });
@@ -51,21 +67,25 @@ export function toggleFavoriteItem(place: FavouritePlace): void {
         false
       )
     ) {
-      localStorage.setItem("favoriteItems",
-        JSON.stringify(localStorageFavouriteItems.filter((item) => item.id !== place.id))
+      localStorage.setItem(
+        "favoriteItems",
+        JSON.stringify(
+          localStorageFavouriteItems.filter((item) => item.id !== place.id)
+        )
       );
       favoritesElements.forEach((el: HTMLElement) => {
-        if (+el.dataset.id === +place.id) {
+        if (String(el.dataset.id) === String(place.id)) {
           el.classList.remove("active");
         }
       });
     } else {
       localStorageFavouriteItems.push(place);
-      localStorage.setItem("favoriteItems",
+      localStorage.setItem(
+        "favoriteItems",
         JSON.stringify(localStorageFavouriteItems)
       );
       favoritesElements.forEach((el: HTMLElement) => {
-        if (+el.dataset.id === +place.id) {
+        if (String(el.dataset.id) === String(place.id)) {
           el.classList.add("active");
         }
       });
@@ -73,10 +93,10 @@ export function toggleFavoriteItem(place: FavouritePlace): void {
   }
 }
 
-export async function bookPlace(
-  id: number,
+export async function bookPlaceHomy(
+  id: number | string,
   searchParams: SearchFormData
-): Promise<void> {
+): Promise<BookResult> {
   const f = await fetch(
     `http://127.0.0.1:3030/places/${id}?checkInDate=${searchParams.startDate}&checkOutDate=${searchParams.endDate}`,
     {
@@ -86,41 +106,91 @@ export async function bookPlace(
       },
     }
   );
-  const d = await f.json();
-  if (d.name === "BadRequest") {
-    renderToast({
-      type: "error",
-      text: "Данные даты не доступны для бронирования",
-    });
+  const bookResult = await f.json();
+
+  if (bookResult.name === "BadRequest") {
+    return { type: "error" };
   } else {
-    renderToast({
+    return {
       type: "success",
-      text: `Отель ${d.name} забронирован на даты с ${new Date(
-        d.bookedDates[0] / 1000
-      )} по ${new Date(d.bookedDates[d.bookedDates.length - 1] / 1000)}`,
-    });
+      checinDate: new Date(searchParams.startDate),
+      checkoutDate: new Date(searchParams.endDate),
+    };
   }
-  console.log(d);
 }
 
-export function renderSearchResultsBlock (results: Place[]): void {
+export async function bookPlaceFlatRent(
+  id: number | string,
+  searchParams: SearchFormData
+): Promise<BookResult> {
+  const newFlatRent = new FlatRentSdk();
+  const bookResult = newFlatRent.book(
+    id,
+    new Date(searchParams.startDate),
+    new Date(searchParams.endDate)
+  );
+  if (!bookResult) {
+    return { type: "error" };
+  } else {
+    return {
+      type: "success",
+      checinDate: new Date(searchParams.startDate),
+      checkoutDate: new Date(searchParams.endDate),
+    };
+  }
+}
+
+export async function bookPlace(
+  id: number | string,
+  searchParams: SearchFormData,
+  provider: Provider
+): Promise<void> {
+  console.log(provider);
+
+  if (provider === Provider.FlatRent) {
+    const bookResult = await bookPlaceFlatRent(id, searchParams);
+    if (bookResult.type === "error") {
+      renderToast({ text: "Отель недоступен для бронирования", type: "error" });
+    }
+    if (bookResult.type === "success") {
+      renderToast({
+        text: `Отель успешно забронирован на даты с ${bookResult.checinDate} по ${bookResult.checkoutDate}`,
+        type: "success",
+      });
+    }
+  }
+  if (provider === Provider.Homy) {
+    const bookResult = await bookPlaceHomy(id, searchParams);
+    if (bookResult.type === "error") {
+      renderToast({ text: "Отель недоступен для бронирования", type: "error" });
+    }
+    if (bookResult.type === "success") {
+      renderToast({
+        text: `Отель успешно забронирован на даты с ${bookResult.checinDate} по ${bookResult.checkoutDate}`,
+        type: "success",
+      });
+    }
+  }
+}
+
+export function getResultsHTML(results: Place[]): string {
   const localStorageFavouriteItems: FavouritePlace[] = JSON.parse(
     localStorage.getItem("favoriteItems")
   );
   let resultsHTML = "";
   results.forEach((result) => {
     const faforiteIsActive = !!localStorageFavouriteItems?.find(
-      (el) => +el.id === +result.id
+      (el) => String(el.id) === String(result.id)
     );
 
     resultsHTML += `<li class="result">
         <div class="result-container">
           <div class="result-img-container">
-            <div class="favorites ${faforiteIsActive ? "active" : ""}"
-            data-id=${result.id}
-            data-name=${result.name}
-            data-img=${result.image}>
-            </div>
+            <div class="favorites ${
+              faforiteIsActive ? "active" : ""
+            }" data-id=${result.id} data-name=${result.name} data-img=${
+      result.image
+    }></div>
             <img class="result-img" src=${result.image} alt="">
           </div>	
           <div class="result-info">
@@ -128,63 +198,37 @@ export function renderSearchResultsBlock (results: Place[]): void {
               <p>${result.name}</p>
               <p class="price">${result.price}&#8381;</p>
             </div>
-            <div class="result-info--map"><i class="map-icon"></i>${result.remoteness}км от вас</div>
+            <div class="result-info--map"><i class="map-icon"></i> ${
+              result.remoteness ? result.remoteness + " км от вас" : ""
+            }</div>
             <div class="result-info--descr">${result.description}</div>
             <div class="result-info--footer">
               <div>
-                <button class="book-btn" data-id=${result.id}>Забронировать</button>
+                <button class="book-btn" data-id=${result.id} data-provider=${
+      result.provider
+    }>Забронировать</button>
               </div>
             </div>
           </div>
         </div>
       </li>`;
   });
-  console.log(resultsHTML);
+  return resultsHTML;
+}
 
-  renderBlock(
-    'search-results-block',
-    `
-    <div class="search-results-header">
-        <p>Результаты поиска</p>
-        <div class="search-results-filter">
-            <span><i class="icon icon-filter"></i> Сортировать:</span>
-            <select>
-                <option selected="">Сначала дешёвые</option>
-                <option selected="">Сначала дорогие</option>
-                <option>Сначала ближе</option>
-            </select>
-        </div>
-    </div>
-    <ul class="results-list">${resultsHTML}</ul>
-    `
-  )
-
-  
-  const favoriteItems = document.getElementsByClassName("favorites");
-  for (let i = 0; i < favoriteItems.length; i++) {
-    favoriteItems[i].addEventListener("click", (e) => {
-      if (e.target instanceof HTMLElement) {
-        toggleFavoriteItem({
-          id: +e.target.dataset.id,
-          name: e.target.dataset.name,
-          img: e.target.dataset.img,
-        });
-      }
-    });
+export function sortResults(results: Place[], type = sortType.cheap): void {
+  let sortedResults: Place[];
+  switch (type) {
+    case sortType.cheap:
+      sortedResults = results.sort((a, b) => a.price - b.price);
+      break;
+    case sortType.expensive:
+      sortedResults = results.sort((a, b) => b.price - a.price);
+      break;
+    case sortType.close:
+      sortedResults = results.sort((a, b) => a.remoteness - b.remoteness);
+      break;
+    default:
   }
-
-  const bookButtons = document.getElementsByClassName("book-btn");
-  console.log(bookButtons);
-
-  for (let i = 0; i < bookButtons.length; i++) {
-    bookButtons[i].addEventListener("click", (e) => {
-      console.log(e);
-
-      if (e.target instanceof HTMLButtonElement) {
-        console.log("book");
-
-        bookPlace(+e.target.dataset.id, collectSearchParams());
-      }
-    });
-  }
+  renderSearchResultsBlock(sortedResults, type);
 }
